9f96b5f37279d4bc93300267900940a0447aeed8
Jeremy Davis <jeremyrdavis@jeremyrdavis.com>
2013-06-09 05:24:22 -0400

    Initial generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummitRailsapp::Application.config.secret_token = '7eb11ac808329dc3a8e11a40bcbb975a2ee3cecd7f45edbb03ef4f440017e20aa275a115b8695a6822b201394eb605224c0786fb1179480e34bf0c660377ee76'
