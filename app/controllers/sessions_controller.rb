d64b0c602c155d832eef2fa4ae50382aa3d10ec6
Jeremy Davis <jeremyrdavis@jeremyrdavis.com>
2013-06-10 09:49:38 -0400

    Add remember_token to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # sign the user in
+      sign_in user
+      redirect_to user
+    else
+      # create an error message
+      flash[:error] = 'Invalid email/password combination'
+      render 'new'
+    end    
   end
 
   def destroy
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610011715) do
+ActiveRecord::Schema.define(:version => 20130610134544) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(:version => 20130610011715) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
