b659dc8c8e6ef425070ebffa9530fd6c582848c1
Jeremy Davis <jeremyrdavis@jeremyrdavis.com>
2013-06-11 07:02:30 -0400

    Switch session store back to cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-#SummitRailsapp::Application.config.session_store :cookie_store, key: '_summit-railsapp_session'
-SummitRailsapp::Application.config.session_store :torquebox_store
+SummitRailsapp::Application.config.session_store :cookie_store, key: '_summit-railsapp_session'
+#SummitRailsapp::Application.config.session_store :torquebox_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
